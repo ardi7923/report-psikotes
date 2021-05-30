419ac3cdf8c83e6850a57405c2b8f3b87121580d
ardi7923 <ardiirawan777@gmail.com>
2021-05-31 00:09:05 +0800

    user profile done

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::group(['middleware' => 'auth'], function () {
 
 	Route::get('dashboard','Admin\DashboardController@index');
 
+	Route::get('user-profile','Admin\UserProfileController@index');
+	Route::post('user-profile/update-user','Admin\UserProfileController@updateUser');
+	Route::post('user-profile/renew-password','Admin\UserProfileController@renewPassword');
+
 	Route::resource('school','Admin\SchoolController');
 	Route::resource('user-school','Admin\UserSchoolController');
 });
