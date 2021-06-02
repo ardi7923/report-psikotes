d8588ab7ac309ab9c642051a21d612013e135c11
ardi7923 <ardiirawan777@gmail.com>
2021-06-02 18:16:55 +0800

    add namespace route

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,23 @@ Route::get('report-result','ReportResultController@index');
 
 require __DIR__.'/auth.php';
 
-Route::group(['middleware' => 'auth'], function () { 
+Route::group(['middleware' => 'auth','namespace' => 'Admin'], function () { 
 
-	Route::get('dashboard','Admin\DashboardController@index');
+	Route::get('dashboard','DashboardController@index');
 
-	Route::get('user-profile','Admin\UserProfileController@index');
-	Route::post('user-profile/update-user','Admin\UserProfileController@updateUser');
-	Route::post('user-profile/renew-password','Admin\UserProfileController@renewPassword');
+	Route::get('user-profile','UserProfileController@index');
+	Route::post('user-profile/update-user','UserProfileController@updateUser');
+	Route::post('user-profile/renew-password','UserProfileController@renewPassword');
 
-	Route::resource('school','Admin\SchoolController');
-	Route::resource('user-school','Admin\UserSchoolController');
+	Route::resource('school','SchoolController');
+	Route::resource('user-school','UserSchoolController');
 
-	Route::get('import-data','Admin\ImportDataController@index');
-	Route::post('import-data','Admin\ImportDataController@store');
+	Route::get('import-data','ImportDataController@index');
+	Route::post('import-data','ImportDataController@store');
 
-	Route::get('result','Admin\ResultController@index');
+	Route::get('result','ResultController@index');
 
-	Route::get('download-example-excel','Admin\DownloadExampleExcelController@download');
+	Route::get('download-example-excel','DownloadExampleExcelController@download');
 
 	// AutoComplete
 	Route::get('autocomplete/school','Autocomplete\AutocompleteSchoolController@get');
